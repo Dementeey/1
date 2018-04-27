136a69e000533e131533b3081f58ac479cfa447f
dementeeyES <metallist9656@gmail.com>
2018-04-27 15:05:06 +0300

    saveES

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const browserSync = require("browser-sync");
 const reload = browserSync.reload;
 const rimraf = require('rimraf');
 const svgmin = require('gulp-svgmin');
+const imageminJpegoptim = require('imagemin-jpegoptim');
 
 const path = {
   build: {                                  // Тут мы укажем куда складывать готовые после сборки файлы
@@ -84,11 +85,13 @@ gulp.task('style:build', function () {
 gulp.task('image:build', function () {
   return gulp.src(path.src.img)             // Выберем наши картинки
     .pipe(imagemin({                        // Сожмем их
-      progressive: true,
-      optimizationLevel: 5,
-      svgoPlugins: [{removeViewBox: true}],
-      use: [pngquant({quality: '60-80', speed: 4})],
-      interlaced: true
+      // progressive: true,
+      // optimizationLevel: 5,
+      use: [
+        pngquant({quality: '60', speed: 5}),
+        imageminJpegoptim({progressive: true, max: 85, stripAll: true})
+      ]
+      // interlaced: true
     }))
     .pipe(gulp.dest(path.build.img))        // И бросим в build
     .pipe(reload({stream: true}));
