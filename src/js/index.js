56fdbc31c72321c5f1fc76e2d2d7c70f55e92c98
dementeeyMac <metallist9656@gmail.com>
2018-06-20 12:15:03 +0300

    add event close menu 'Esc', add style .scroll & info for  right card

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,28 @@ const burger    = document.querySelector('.burger-wrap');
 const closeMenu = document.querySelector('.close-menu');
 const menu      = document.querySelector('.menu');
 
+/* open/close menu logic */
+
 burger.addEventListener('click', function (e) {
   e.preventDefault();
   menu.classList.remove('is-closed');
 })
+
 closeMenu.addEventListener('click', function (e) {
   e.preventDefault();
   menu.classList.add('is-closed');
 })
+
+closeMenu.addEventListener('click', function (e) {
+  e.preventDefault();
+  menu.classList.add('is-closed');
+})
+
+window.addEventListener('keydown', function (e) {
+  const keyCode = event.keyCode || event.which;
+	if (keyCode === 27) {
+    event.preventDefault();
+    menu.classList.add('is-closed');
+	}
+})
+/*------------------------------- end -------------------------------*/
