b76698634426de0bb09efad7718da23880f4e667
dementeeyMac <metallist9656@gmail.com>
2018-06-22 09:44:44 +0300

    add overflow hidden body - active menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,33 @@
 /* open/close menu logic */
+
+const body        = document.querySelector('body');
 const burger      = document.querySelector('.burger-wrap');
 const closeMenu   = document.querySelector('.close-menu');
 const menu        = document.querySelector('.menu');
 
 const onClickOpenMenu = () => {
   menu.classList.remove('is-closed');
-
+  body.classList.add('menu--active')
 }
 
 const onClickCloseMenu = () => {
   menu.classList.add('is-closed');
+  body.classList.remove('menu--active')
 
   window.addEventListener('keydown', function (e) {
     const keyCode = event.keyCode || event.which;
     if (keyCode === 27) {
       event.preventDefault();
       menu.classList.add('is-closed');
+      body.classList.remove('menu--active')
     }
   })
 }
 
 burger.onclick = onClickOpenMenu;
 closeMenu.onclick = onClickCloseMenu;
+
+
 
 /*------------------------------- end -------------------------------*/
 
